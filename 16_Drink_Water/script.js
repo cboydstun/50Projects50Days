6dc3ae1d3441c89d295491777d1af3c4ca9a6ac9
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2020-12-23 10:06:26 -0600

    updated Cup JS

--- 16_Drink_Water/script.js
+++ 16_Drink_Water/script.js
@@ -20,4 +20,29 @@ function highlightCups(idx){
             cup.classList.remove('full')
         }
     })
+
+    updateBigCup()
+}
+
+function updateBigCup(){
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    
+    const totalCups = smallCups.length
+
+    if(fullCups===0){
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups/totalCups * 330}px`
+        percentage.innerText = `${fullCups/totalCups * 100}%`
+    }
+
+    if(fullCups===totalCups){
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible'
+        liters.innerText = `${2-(250*fullCups / 1000)}L`
+    }
 }
