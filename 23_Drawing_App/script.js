42fd68846096c94554300f95deac8ba697b250d9
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2020-12-30 08:33:23 -0600

    finished toolbar JS

--- 23_Drawing_App/script.js
+++ 23_Drawing_App/script.js
@@ -1,7 +1,13 @@
 const canvas = document.getElementById('canvas')
+const increaseBtn = document.getElementById('increase')
+const decreaseBtn = document.getElementById('decrease')
+const sizeEl = document.getElementById('size')
+const colorEl = document.getElementById('color')
+const clearEl = document.getElementById('clear')
+
 const ctx = canvas.getContext('2d')
 
-let size = 20
+let size = 10
 let isPressed = false
 let color = 'black'
 let x
@@ -50,3 +56,32 @@ function drawLine(x1, y1, x2, y2){
     ctx.stroke()
 }
 
+function updateSizeOnScreen(){
+    sizeEl.innerText = size
+}
+
+increaseBtn.addEventListener('click', ()=>{
+    size += 5
+
+    if(size > 50){
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', ()=>{
+    size -= 5
+
+    if(size < 5){
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e)=>{
+    color = e.target.value
+})
+
+clearEl.addEventListener('click', ()=>ctx.clearRect(0, 0, canvas.width, canvas.height))
