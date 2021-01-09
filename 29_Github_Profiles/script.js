9cca211ba5cd5f3c6a493aee67eb653191591c48
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-01-09 08:29:00 -0600

    request specific user data

--- 29_Github_Profiles/script.js
+++ 29_Github_Profiles/script.js
@@ -1,6 +1,8 @@
 const APIURL = 'https://api.github.com/users/'
 
-getUser('cboydstun')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
 
 async function getUser(username){
     try{
@@ -10,3 +12,15 @@ async function getUser(username){
         console.log(err)
     }
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user){
+        getUser(user)
+
+        search.value = ''
+    }
+})
