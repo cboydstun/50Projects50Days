8de97dc42e41cfa09f4bf4b484c0f9123d5bd75c
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-01-21 06:17:03 -0600

    added local storage

--- 33_Notes_App/script.js
+++ 33_Notes_App/script.js
@@ -1,18 +1,24 @@
 const addBtn = document.getElementById('add')
 
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if(notes) {
+    notes.forEach(note => addNewNote(note))
+}
+
 addBtn.addEventListener('click', () => addNewNote())
 
-function addNewNote(text = ''){
+function addNewNote(text = '') {
     const note = document.createElement('div')
     note.classList.add('note')
 
     note.innerHTML = `
-        <div class="tools">
-            <button class="edit"><i class="fas fa-edit"></i></button>
-            <button class="delete"><i class="fas fa-trash-alt"></i></button>
-        </div>
-        <div class="main ${text ? "" : "hidden"}"></div>
-        <textarea class="${text ? "hidden" : ""}"></textarea>    
+    <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+    <div class="main ${text ? "" : "hidden"}"></div>
+    <textarea class="${text ? "hidden" : ""}"></textarea>
     `
 
     const editBtn = note.querySelector('.edit')
@@ -25,6 +31,8 @@ function addNewNote(text = ''){
 
     deleteBtn.addEventListener('click', () => {
         note.remove()
+
+        updateLS()
     })
 
     editBtn.addEventListener('click', () => {
@@ -33,14 +41,22 @@ function addNewNote(text = ''){
     })
 
     textArea.addEventListener('input', (e) => {
-        const {value} = e.target
+        const { value } = e.target
+
         main.innerHTML = marked(value)
+
+        updateLS()
     })
 
     document.body.appendChild(note)
 }
 
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea')
 
+    const notes = []
 
+    notesText.forEach(note => notes.push(note.value))
 
-
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
