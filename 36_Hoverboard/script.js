9dc6a89246d7d37593bd5a2aa7e1709c375469dd
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-01-28 08:18:24 -0600

    changed colors

--- 36_Hoverboard/script.js
+++ 36_Hoverboard/script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container')
-const colors = ['red', 'green', 'blue', 'yellow', 'purple']
-const SQUARES = 5000
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const SQUARES = 500
 
 for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
